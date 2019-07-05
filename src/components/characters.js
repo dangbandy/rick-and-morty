04a5a0fbabf77dbb457695a56ad467648bf64e62
dangbandy <dangbandy@gmail.com>
2019-07-04 21:53:09 -0700

    character page almost done

--- src/components/characters.js
+++ src/components/characters.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Container from "react-bootstrap/es/Container";
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
+import ListGroup from "react-bootstrap/ListGroup";
+import Spinner from "react-bootstrap/Spinner";
 import Col from "react-bootstrap/Col";
+
 class Characters extends Component {
-    items =[];
+    allCharacters=[];
+    pages = [];
     constructor(props) {
         super(props);
         this.state = {
@@ -16,50 +19,72 @@ class Characters extends Component {
     }
 
     componentDidMount() {
-        fetch("https://rickandmortyapi.com/api/character/")
-            .then(res => res.json())
-            .then((result) => {
-                    this.setState({
-                        isLoaded: true,
-                        chars: result.results
-                    });
-                },
-                // Note: it's important to handle errors here
-                // instead of a catch() block so that we don't swallow
-                // exceptions from actual bugs in components.
-                (error) => {
-                    this.setState({
-                        isLoaded: true,
-                        error
-                    });
-                }
-            )
+        this.pages = [];
+        for(let i=1; i<= 20;i++){
+            fetch("https://rickandmortyapi.com/api/character/?page="+i)
+                .then(res => res.json())
+                .then((result) => {
+                        this.pages.push(i);
+                        this.setState({
+                            isLoaded: true,
+                            chars: result.results
+                        });
+                    },
+                    // Note: it's important to handle errors here
+                    // instead of a catch() block so that we don't swallow
+                    // exceptions from actual bugs in components.
+                    (error) => {
+                        this.setState({
+                            isLoaded: true,
+                            error
+                        });
+                    }
+                ).then(()=>{
+                    this.populate(this.state.chars);
+            })
+        }
+
     }
 
 
 
-    populate(){
-        for (let number = 1; number <= 25; number++) {
-            this.items.push(
-                <span>
+    populate(chars){
+        this.allCharacters.push(
+            chars.map(char => (
+                    <span>
                     <Card className="char-card">
-                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Header>{char.name}</Card.Header>
+                        <Card.Img variant="top" src={char.image}/>
                         <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
+                                <ListGroup>
+                                  <ListGroup.Item className="char-list-item">
+                                      Status: {char.status}
+                                  </ListGroup.Item>
+                                  <ListGroup.Item className="char-list-item">
+                                      Species: {char.species}
+                                  </ListGroup.Item>
+                                  <ListGroup.Item className="char-list-item">
+                                      Type: {char.type}
+                                  </ListGroup.Item>
+                                  <ListGroup.Item className="char-list-item">
+                                      Gender: {char.gender}
+                                  </ListGroup.Item>
+                                  <ListGroup.Item className="char-list-item">
+                                      Origin: {char.origin.name}
+                                  </ListGroup.Item>
+                                  <ListGroup.Item className="char-list-item">
+                                      Location: {char.location.name}
+                                  </ListGroup.Item>
+                                </ListGroup>
                         </Card.Body>
                     </Card>
-                </span>
-            );
-        }
-        return this.items;
+                    </span>
+                ))
+        );
+        return this.allCharacters;
     }
 
-    render() /*{
+    render()/* {
         return (
             <section className="characters-content">
                 <Container className="char-container">
@@ -73,20 +98,27 @@ class Characters extends Component {
         )
     }*/
     {
-        const { error, isLoaded, chars } = this.state;
+        const { error } = this.state;
         if (error) {
+            alert("error");
             return <div>Error: {error.message}</div>;
-        } else if (!isLoaded) {
-            return <div>Loading...</div>;
+        } else if ( this.pages.length < 20 ) {
+            return (
+                <Container className="char-loading">
+                            <Spinner animation="border" role="status">
+                                <span className="sr-only">Loading...</span>
+                            </Spinner>
+                </Container>
+            );
         } else {
             return (
-                <ul>
-                    {chars.map(char => (
-                        <li key={char.name}>
-                            {char.name} {char.price}
-                        </li>
-                    ))}
-                </ul>
+                <section className="characters-content">
+                    <Container className="char-container">
+                        <Row className="char-row">
+                            {this.allCharacters}
+                        </Row>
+                    </Container>
+                </section>
             );
         }
     }
